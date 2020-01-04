59472920a0ca0063502585370ba0f52b4d4f5ebb
Kevin <kvmo@demant.com>
2020-01-04 15:22:51 -0500

    Updates

---
+++ precache-manifest.ada03a8e9b0186c250ca2dc4cafb398f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9f34194f7c48bfa541d8cb3066e087",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3e0cfc51fcb973e6d95a",
+    "url": "/monsters-rolodex/static/css/main.02671bcf.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e0cfc51fcb973e6d95a",
+    "url": "/monsters-rolodex/static/js/main.bddb64f1.chunk.js"
+  },
+  {
+    "revision": "dc25ba12107eb546721c",
+    "url": "/monsters-rolodex/static/js/runtime-main.75c8e44f.js"
+  }
+]);
